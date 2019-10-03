edaa52087790701dd00f5b065bcbad6817aedb20
soaresmju <ju.soares.moraes@outlook.com>
2019-10-03 17:50:06 -0300

    continuando

--- Desktop/routerju/routes/web.php
+++ Desktop/routerju/routes/web.php
@@ -11,14 +11,20 @@
 |
 */
 
-Route::get('/', function () {
-    return '<h1>Caminho 1</h1> <a href="./c2"><button type="" > Próximo</button></a>';
-});
 
-Route::get('/c2', function () {
-    return '<h2>Caminho 2</h2> <a href="./"><button type="" >Voltar</button></a>   <a href ="./c3"><button type="" > Próximo</button></a>';
-});
+Route::group(['prefix' => 'exercicio'], function(){
 
-Route::get('/c3', function () {
-    return '<h3>Caminho 3</h3> <a href="./"><button> Caminho 1</button></a>    <a href="./c2"><button> Caminho 2</button></a>';
+    Route::get('/', function () {
+        return '<h1>Caminho 1</h1> <a href="./c2"><button type="" > Próximo</button></a>';
+    });
+    
+    Route::get('/
+    c2', function () {
+        return '<h2>Caminho 2</h2> <a href="./"><button type="" >Voltar</button></a>   <a href ="./c3"><button type="" > Próximo</button></a>';
+    });
+    
+    Route::get('/c3', function () {
+        return '<h3>Caminho 3</h3> <a href="./"><button> Caminho 1</button></a>    <a href="./c2"><button> Caminho 2</button></a>';
+    });
+
 });
